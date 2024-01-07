01cb04dd95cb5ae09c1cc08c8f0c938ed084b075
TulioHRC <grove1411@gmail.com>
2024-01-07 14:34:08 -0300

    feat: Frontend components and fetch OMDb API

---
+++ frontend/src/Components/LibraryPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function LibraryPage() {
+  return (
+    <div>
+        <h2>Library</h2>
+        <Link to="/">Main</Link>
+    </div>
+  )
+}
+
+export default LibraryPage
