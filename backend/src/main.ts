0c0e8760af9e87de42c2740a209a4782ca33cba8
TulioHRC <grove1411@gmail.com>
2024-01-07 11:43:48 -0300

    feat: .env configure keys backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,8 @@
 import { NestFactory } from '@nestjs/core';
+import { config } from 'dotenv';
 import { AppModule } from './app.module';
+
+config(); // ENV keys (like the database key)
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
