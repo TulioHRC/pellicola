57b2ac872721eb9413c2a088039fc833e868486e
TulioHRC <grove1411@gmail.com>
2024-01-09 20:31:11 -0300

    feat: Client request for API

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,14 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { config } from 'dotenv';
 import { AppModule } from './app.module';
+import * as cors from 'cors'
 
 config(); // ENV keys (like the database key)
 
 const PORT = process.env.PORT || 3001 
 
+const corsOptions = {
+    origin: process.env.FRONTEND_URL,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE'
+}
+
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    app.enableCors();
+    app.use(cors(corsOptions));
     await app.listen(PORT);
     console.log(`Listening on ${PORT}`)
 }
