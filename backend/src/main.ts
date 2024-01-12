8eb660a15871b731cb2ce9948349175153cdf173
TulioHRC <grove1411@gmail.com>
2024-01-12 11:11:31 -0300

    test: cors liberation

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,20 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { config } from 'dotenv';
 import { AppModule } from './app.module';
-import * as cors from 'cors'
+//import * as cors from 'cors'
 
 config(); // ENV keys
 
 const PORT = process.env.PORT || 3001; 
 
-const corsOptions = {
+/*const corsOptions = {
     origin: process.env.FRONTEND_URL,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE'
-}
+}*/
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    app.use(cors(corsOptions));
+    //app.use(cors(corsOptions));
     await app.listen(PORT);
     console.log(`Listening on ${PORT}`);
 }
